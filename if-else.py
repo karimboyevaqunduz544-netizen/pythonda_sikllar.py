c4da5d13e33c4799912302765ef75f8811d9fdda
Qunduz Karimboyeva <karimboyevaqunduz544@gmail.com>
2026-08-11 10:06:46 +0500

    Create if-else.py

---
+++ if-else.py
@@ -0,0 +1,23 @@
+#if-else in Python
+#Weather
+weather = "sunny"
+if weather=="sunny":
+	print("It's reall sunny today!")
+else:
+	print("There is no sun today.")
+
+	#It's reall sunny today!
+
+	#Exam Score
+score = 75
+if score >= 90:
+	print("You got on A!")
+elif score >= 80:
+	print("You got a B!")
+elif score >= 70:
+	print("You got a C.")
+elif score >= 60:
+	print("You got a D.")
+else:
+	print("You failed!")
+	#You got a C.
