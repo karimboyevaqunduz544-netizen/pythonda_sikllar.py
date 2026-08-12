0fdd966ed97e59883f1250ddef907f1b945798cb
Qunduz Karimboyeva <karimboyevaqunduz544@gmail.com>
2026-08-12 07:54:04 +0500

    Create If_sharti.py

---
+++ If_sharti.py
@@ -0,0 +1,8 @@
+if condition: #agar sharti (shart rost bo‘lganda bajariladigan kod)
+
+age = 20 #age degan ozgaruvchiga 20 qiymat berdik
+if age >= 18: #agar age 18dan kichik yoki teng bolsa
+	print("You are an adult.") # Siz kattasizni chiqar
+	Output:
+	#You are an adult.
+	
