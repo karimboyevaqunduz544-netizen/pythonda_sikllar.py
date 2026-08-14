74dfe6000d1f485e3432020d4a68d1ec86d817d5
Qunduz Karimboyeva <karimboyevaqunduz544@gmail.com>
2026-08-15 00:21:57 +0500

    Create if_elif_else2.py

---
+++ if_elif_else2.py
@@ -0,0 +1,9 @@
+score = 78 #score o'zgaruvchiga 78 qiymat berdik'
+if score>90: #agar score 90 dan katta bo'lsa'
+	print("A grade") # A gradeni chiqar
+elif score > 75 and score < 90 :#aks holda score 75dan katta va  90 dan kichik bo'lsa'
+	print("B grade") # B gradeni chiqar
+else: # ikkalasi ham bo"lmasa buni sinab ko'r
+    print("Keep Trying") #Keep Tryingni chiqar
+	Output:
+		B grade
