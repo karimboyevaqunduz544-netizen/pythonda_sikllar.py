7920de9ce3b8b7e2bad6ce60d8f57466cf49b747
Qunduz Karimboyeva <karimboyevaqunduz544@gmail.com>
2026-08-17 00:57:53 +0500

    Create if elif else-day.py

---
+++ if elif else-day.py
@@ -0,0 +1,19 @@
+d = int(input("enter day:")) #foydalanuvchidan qiymat kiritishini soraymiz
+if d == 1: #agar d 1ga teng bolsa
+	print("monday") #dushanbani chiqar
+elif d == 2: #aks holda d 2 ga teng bolsa
+	print("tuesday") #Seshanbani chiqar
+elif d == 3: #aks holda d 3 ga teng bolsa
+	print("wednesday") #chorshanbani chiqar
+elif d == 4: #aks holda d 4 ga teng bolsa
+	print("thursday") #payshanbani chiqar
+elif d == 5: #aks holda d 5ga teng bolsa
+	print("friday") #Jumani chiqar
+elif d ==6: #aks holda d 6ga teng bolsa
+	print("saturday") #shanbani chiqar
+elif d == 7: #aks holda d 7ga teng bolsa
+	print("sunday") #Yakshanbani chiqar
+else:#agar hech biri bolmasa buni sinab kor
+	print("wrong day")#notogri kun
+	Output:
+		#enter day: 1 monday
