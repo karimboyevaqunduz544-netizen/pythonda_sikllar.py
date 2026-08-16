4f80a715f841e504ce8da2fc5cbd80c0de8a5b52
Qunduz Karimboyeva <karimboyevaqunduz544@gmail.com>
2026-08-16 06:26:24 +0500

    Create if_else(input).py

---
+++ if_else(input).py
@@ -0,0 +1,11 @@
+a = int(input("Enter age:")) # foydalanuvchidan qiymat  kiritishini so'raymiz
+if (a < 18): # agar a 18dan kichik bo'lsa'
+	print("You cant access") #siz kira olmaysiz
+else: #aks holda
+	print("You can access") #Siz kira olasiz
+Output:
+"""
+		Enter age:20
+You can access
+
+"""
