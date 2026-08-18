f4845eea7f66ed6e9ac32a8377d2befde9495764
Qunduz Karimboyeva <karimboyevaqunduz544@gmail.com>
2026-08-18 07:10:33 +0500

    Create if-else_inpython.py

---
+++ if-else_inpython.py
@@ -0,0 +1,8 @@
+a = 23 #a o'zgaruvchiga 23 qiymat berdik
+b = 100 #b o'zgaruvchiga 100 qiymat berdik'
+if a < b: #agar a kichik b dan bo'lsa
+	print("b adan katta") #b adan kattani chiqar
+else: # unday bolmasa buni sinab kor
+	print("b a dan kichik")#b adan kichik
+	Output:
+		#b adan katta
